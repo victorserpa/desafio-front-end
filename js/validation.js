926af0ad8c36973f66977aa690a552037f4b5dd1
Victor Schmucker Serpa <schmvitoor@gmail.com>
2022-06-08 21:09:42 -0300

    Melhorias

---
+++ js/validation.js
@@ -0,0 +1,16 @@
+function validaUsuar() {
+
+    if (document.login.usuario.value != "teste") {
+        alert("Preencha corretamente o seu usuário")
+        document.login.usuario.focus();
+        return false;
+    }
+    
+    if(document.login.password.value != "teste"){
+        alert("Preencha corretamente a sua senha!")
+        document.login.password.focus();
+        return false;
+    }
+    
+    return true;
+}
